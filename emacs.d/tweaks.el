d4726f705d46d3dd5c078e77fcc624a65aa37a9d
Matt Moriarity <matt.moriarity@gmail.com>
2008-10-23 15:46:21 -0400

    No command as meta, and change the frame title

--- emacs.d/tweaks.el
+++ emacs.d/tweaks.el
@@ -1,9 +1,16 @@
-(setq mac-command-modifier 'meta)
 (fset 'yes-or-no-p 'y-or-n-p)
 
 (ido-mode)
 
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
+
+(setq frame-title-format 
+      '(:eval 
+        (if buffer-file-name 
+            (replace-regexp-in-string 
+             (getenv "HOME") "~"
+             (file-name-directory buffer-file-name)) 
+          (buffer-name))))
 
 ;;; A stolen function for counting words
 (defun word-count nil "Count words in buffer"

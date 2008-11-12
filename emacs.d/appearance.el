fc521bc1279c6cc9b28a764770efaf3f012bfe06
Matt Moriarity <matt.moriarity@gmail.com>
2008-11-12 14:17:52 -0500

    No longer mess with window dimensions on start

--- emacs.d/appearance.el
+++ emacs.d/appearance.el
@@ -5,9 +5,9 @@
 (set-terminal-coding-system 'iso-8859-1)
 (set-face-attribute 'default nil
                     :family "consolas" :height 130)
-(setq default-frame-alist '((width . 168)
-                            (height . 56)
-                            (top . 0)
-                            (left . 0)
+(setq default-frame-alist '(;(width . 168)
+                            ;(height . 56)
+                            ;(top . 0)
+                            ;(left . 0)
                             (tool-bar-line . 0)
                             (cursor-type . bar)))

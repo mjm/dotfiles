677705c97773db6e173cafd784c3ce66b8c5f15a
Matt Moriarity <matt.moriarity@gmail.com>
2008-11-25 14:59:22 -0500

    Added shortcuts for moving between windows Also changed the way most shortcuts are specified to make it clearer what the shortcut is. Only abstained if the new way didn't actually work

--- emacs.d/shortcuts.el
+++ emacs.d/shortcuts.el
@@ -1,16 +1,20 @@
-(global-set-key "\C-w" 'backward-kill-word)
-(global-set-key "\C-x\C-k" 'kill-region)
-(global-set-key "\C-c\C-k" 'kill-region)
+(global-set-key [(control w)] 'backward-kill-word)
+(global-set-key [(control x) (control k)] 'kill-region)
+(global-set-key [(control c) (control k)] 'kill-region)
 
-(global-set-key "\C-x\C-u" 'undo)
+(global-set-key [(control x) (control u)] 'undo)
 
-(global-set-key "\C-x\C-b" 'ido-switch-buffer)
+(global-set-key [(control x) (control b)] 'ido-switch-buffer)
 
 (global-set-key "\M- " 'set-mark-command)
 (global-set-key "\r" 'newline-and-indent)
-(global-set-key "\C-j" 'newline)
-(global-set-key [M-S-return] 'find-file-at-point)
+(global-set-key [(meta shift return)] 'find-file-at-point)
 
-(global-set-key "\C-cy" 'clipboard-yank)
+(global-set-key [(control c) (y)] 'clipboard-yank)
 
-(global-set-key "\C-cm" 'magit-status)
+(global-set-key [(control c) (m)] 'magit-status)
+
+(global-set-key [(control x) (left)] 'windmove-left)
+(global-set-key [(control x) (right)] 'windmove-right)
+(global-set-key [(control x) (up)] 'windmove-up)
+(global-set-key [(control x) (down)] 'windmove-down)

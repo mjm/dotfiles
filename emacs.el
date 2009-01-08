bdcf564421a62decf8e30848f0d07ebdd4fb89dd
Matt Moriarity <matt.moriarity@gmail.com>
2009-01-08 18:39:26 -0500

    Switched to the new .emacs file

--- emacs.el
+++ emacs.el
@@ -1,41 +1,7 @@
-;;; Start the Emacs server so we can use emacsclient
-(server-start)
+(load "~/.emacs.d/pre-init.el")
 
-;;; Avoid custom polluting the .emacs file
-(setq custom-file "~/.emacs-custom.el")
-(load custom-file 'noerror)
-
-(setq dotfiles-dir "~/.emacs.d/")
-(add-to-list 'load-path dotfiles-dir)
-(add-to-list 'load-path (concat dotfiles-dir "elpa"))
-(add-to-list 'load-path "~/usr/share/elpa-alt")
-(setq autoload-file (concat dotfiles-dir "loaddefs.el"))
-
-;;; Set the path to be right
-(setenv "PATH" (concat "/usr/local/bin:/usr/texbin:" (getenv "PATH")))
-(add-to-list 'exec-path "/usr/local/bin")
-(add-to-list 'exec-path "/usr/texbin")
-
-;;; Load things that will used often
-(require 'cl)
-(require 'saveplace)
-(require 'ffap)
-(require 'uniquify)
-(require 'ansi-color)
-(require 'recentf)
-(require 'imenu)
-
-;;; Load the package manager
-(require 'package)
-(package-initialize)
-
-(setq files-to-load
-      '(misc defuns clojure lisp slime shortcuts ruby
-        rails matlab markdown appearance blogmax eshell org))
-
-(defun load-config (file)
-  (require (intern (concat "moriarity-" (symbol-name file)))))
-
-(mapcar 'load-config files-to-load)
-
-(regen-autoloads)
+;;; Automatically load any ~/.emacs.d/init-*.el files
+(dolist (file (sort (file-expand-wildcards
+		     (concat init-dir "init-*.el"))
+		    #'string<))
+  (load (file-name-sans-extension file) t))

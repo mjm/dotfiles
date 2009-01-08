54b40a6f4f4ae51ada72dfaa9cc8e5ae098c0018
Matt Moriarity <matt.moriarity@gmail.com>
2009-01-07 23:41:50 -0500

    Added slime configuration (preliminary)

---
+++ emacs.d/lisp/init-slime.el
@@ -0,0 +1,25 @@
+;;; Set the load path for slime, which is maintained through clbuild
+(setq clbuild-dir (expand-file-name "~/lisp/clbuild/"))
+(add-to-list 'load-path (concat clbuild-dir "source/slime"))
+(add-to-list 'load-path (concat clbuild-dir "source/slime/contrib")))
+
+(require 'slime-autoloads)
+
+(eval-after-load "slime"
+  '(progn
+     (setq inferior-lisp-program "sbcl"
+	   slime-complete-symbol*-fancy t
+	   slime-complete-symbol-function 'slime-fuzzy-complete-symbol
+	   slime-when-complete-filename-expand t
+	   slime-truncate-lines nil
+	   slime-autodoc-use-multiline-p t)
+
+     (slime-setup '(slime-fancy slime-asdf))
+
+     (global-set-key (kbd "C-c s") 'slime-selector)))
+
+(add-hook 'lisp-mode-hook
+	  (lambda ()
+	    (cond ((not (featurep 'slime))
+		   (require 'slime)
+		   (normal-mode)))))

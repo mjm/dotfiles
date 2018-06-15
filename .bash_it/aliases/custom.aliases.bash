f84d3c859ddbc073f696ff875282f647a7ce4822
Matt Moriarity <matt@mattmoriarity.com>
2018-06-15 15:48:30 -0500

    Add some blogging aliases
    Also some extra alias cleanup

--- .bash_it/aliases/custom.aliases.bash
+++ .bash_it/aliases/custom.aliases.bash
@@ -7,11 +7,13 @@ alias squish="git status && git commit -a --amend -C HEAD"
 alias swb="git branch | cut -c 3- | selecta | xargs git checkout"
 alias https="http --default-scheme=https --verify=no"
 
-alias sedit="xcrun sedit"
-
 alias og="ag --objc --objcpp"
 alias win='tmux rename-window $(basename `pwd`)'
 alias xc='open -a $(dirname $(dirname $(xcode-select -p)))'
+
+alias bedit="vi \$(find . -name '*.md' | selecta)"
+alias vialias="vi ~/.bash_it/aliases/custom.aliases.bash"
+alias toot="pressy write -s publish"
 
 current_branch() {
   git rev-parse --abbrev-ref HEAD
